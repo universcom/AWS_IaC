8177e726d642f93a67a4d5da7cd8708d31d8b61f
Hamed Enayat <hamed.enayatzare@gmail.com>
2024-06-17 19:50:47 +0330

    Remove additional variables

--- VPC/Create_VPC/variables.tf
+++ VPC/Create_VPC/variables.tf
@@ -1,20 +1,20 @@
-variable "aws_region" {
-  description = "The AWS region to create resources in."
-  type        = string
-}
+# variable "aws_region" {
+#   description = "The AWS region to create resources in."
+#   type        = string
+# }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type        = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "cidr_block" {
+variable "vpc_cidr_block" {
     description = "value"
     type        = string
 }
